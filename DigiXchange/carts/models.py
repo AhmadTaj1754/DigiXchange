8254d8df2fcffbc11c0aa692bbf9b320810b40d4
Ahmad Tajgardoun <ahmadtaj@Ahmads-MBP.fios-router.home>
2019-02-28 12:38:03 -0500

    various clean up in carts and search apps

--- DigiXchange/carts/models.py
+++ DigiXchange/carts/models.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save, post_save, m2m_changed
@@ -64,7 +63,7 @@ m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
 
 def pre_save_cart_receiver(sender, instance, *args, **kwargs):
     if instance.subtotal > 0:
-        instance.total = Decimal(instance.subtotal) * Decimal(1.08) # 8% tax
+        instance.total = instance.subtotal  + 10 #* 1.08
     else:
         instance.total = 0.00
 
---
+++ Readme.md
@@ -0,0 +1,11 @@
+User Story: I can add and remove products from the cart
+
+User Story: I can register as a user
+
+User Story: I can save my user cart history
+
+User Story: I can save my personal information
+
+User Story: I can save my credit card information
+
+User Story: I can view product details
